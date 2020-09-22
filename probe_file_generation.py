1bd9901f5f17699bbd820b7d828bed1cf5174daf
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-09-22 10:56:44 +0200

    probe_file_generation.py works as it should and spyking circus has an approbiate probe file

--- probe_file_generation.py
+++ probe_file_generation.py
@@ -1,5 +1,7 @@
 import h5py
 import numpy as np
+import _pickle as pickle
+import circus
 from IPython import embed
 
 filepath = r'D:\Lisa\data\Andrea\AAV5-CamKII-PSAM-HA\Slice 1\2020-06-16T13-11-53AAV5PSAMCamIIGFP KCl9.6mM.h5'
@@ -8,17 +10,17 @@ file = h5py.File(filepath, 'r')
 
 labels = [ch[4].decode('utf8') for ch in file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']]
 same_len_labels = [str(label[0]) + '0' + str(label[1]) if len(label) < 3 else label for label in labels]
-ch_ids = [ch[0] for ch in file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']]
+ch_ids = [ch[1] for ch in file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']]
 indices = list(np.argsort(same_len_labels))
-
+sorted_channel_ids = np.array(ch_ids)[indices]
 spacing = 38
 radius = 4
 
 geometry = []
 
 for i in range(16):
-    for j in range(16):
-        if i == 0 and j == 0 or i == 0 and j == 15 or i == 15 and j == 0 or i == 15 and j == 15:
+    for j in reversed(range(16)):
+        if (i == 0 and j == 0) or (i == 0 and j == 15) or (i == 15 and j == 0) or (i == 15 and j == 15):
             continue
         x = radius + i * spacing
         y = radius + j * spacing
@@ -26,4 +28,21 @@ for i in range(16):
         current_electrode = (x, y)
         geometry.append(current_electrode)
 
+geometry_probe = dict()
+for ch_id, geo in zip(np.array(ch_ids)[indices], np.array(geometry)[indices]):
+    geometry_probe[ch_id] = tuple(geo)
 embed()
+
+fout = "probe.txt"
+fo = open(fout, "w")
+
+for k, v in geometry_probe.items():
+    fo.write(str(k) + ': ' + str(v) + ', \n')
+
+fo.close()
+
+
+#
+# print(sorted_channel_ids-2)
+# savedict.write(str(geometry_probe))
+# savedict.close()
