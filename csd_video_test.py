93a448ea5f1b45e0640da255e177398baaccd4f8
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-12-16 17:27:17 +0100

    * starting with the video approach of the csd data

---
+++ csd_video_test.py
@@ -0,0 +1,9 @@
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+mea_recording_path = '/home/mea_recordings/CSD_data/'
+video_path = '/home/mea_recordings/CSD_data'
+
+
