fc1d92a4548d05144cdbf9b7e17989876e27981b
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2021-01-06 11:25:12 +0100

    starting to implement scale bar and to set ylims nicely. Therefore I have to go through the given data and pick the right maxima and minima (not the artefact of the puff)

--- csd_video_test.py
+++ csd_video_test.py
@@ -1,9 +1,11 @@
 import h5py
 import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.anchored_artists import AnchoredSizeBar
 import tifffile as tiff
 import numpy as np
 import time
+
 matplotlib.use("agg")
 
 
@@ -40,10 +42,10 @@ def get_channel_id(label):
             return ch[1]
 
 
-mea_recording_path = r'C:\Users\Lisa\Desktop\mea_csd\h5_data' \
-                     r'\2020-10-13T14-27-38FHM3_GS967_BL6_P15_male_400ms_7psi_Slice3_Test1_WITH.h5'
-video_path = r'C:\Users\Lisa\Desktop\mea_csd\201013_microscope' \
-             r'\FHM3_GS967_BL6_P15_male_exp100ms_dur100sec_400ms_7psi_Slice3_Test1_WITH_4movie.tif'
+mea_recording_path = "/home/lisa_ruth/mea_recordings/CSD_data/h5_data" \
+                     "/2020-10-13T14-27-38FHM3_GS967_BL6_P15_male_400ms_7psi_Slice3_Test1_WITH.h5"
+video_path = "/home/lisa_ruth/mea_recordings/CSD_data/201013_microscope" \
+             "/FHM3_GS967_BL6_P15_male_exp100ms_dur100sec_400ms_7psi_Slice2_Test3_4movie.tif"
 
 mea_file = h5py.File(mea_recording_path, 'r')
 t0 = time.time()
@@ -65,8 +67,8 @@ t3 = time.time() - t2
 print('time to open tiff stack:', t3)
 print(mats.shape)
 
-relevant_labels = ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'E3',  'E4',
-                   'E5', 'E6', 'E7', 'E8', 'E9', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9',  'G3', 'G4', 'G5', 'G6',
+relevant_labels = ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'E3', 'E4',
+                   'E5', 'E6', 'E7', 'E8', 'E9', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'G3', 'G4', 'G5', 'G6',
                    'G7', 'G8', 'G9', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9',
                    'K3', 'K4', 'K5', 'K6', 'K7', 'K8', 'K9', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'M3', 'M4', 'M5',
                    'M6', 'M7', 'M8', 'M9', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9']
@@ -80,43 +82,28 @@ num_frames = len(mats)
 timepoints = np.arange(stepsize, (num_frames * stepsize) + 1, stepsize)
 
 for i, mat in enumerate(mats):
-    if i < 317:
-        continue
     fig = plt.figure(figsize=(12, 8))
     plt.imshow(mat, cmap='gray', interpolation='none')
-    # ax_0 = fig.add_axes([0.205, 0.6725, 0.05, 0.05])
-    # ax_0.plot(voltage_traces[channel_ids[33]][:1000], alpha=0.25)
-    # ax_0.spines['right'].set_visible(False)
-    # ax_0.spines['top'].set_visible(False)
-    # ax_0.spines['left'].set_visible(False)
-    # ax_0.spines['bottom'].set_visible(False)
-    # ax_0.axes.get_xaxis().set_visible(False)
-    # ax_0.axes.get_yaxis().set_visible(False)
-    # ax_0.axes.get_xaxis().set_ticks([])
-    # ax_0.axes.get_yaxis().set_ticks([])
-    # ax_0.patch.set_alpha(0.0)
-    #
-    # ax_1 = fig.add_axes([0.205, 0.5725, 0.05, 0.05])
-    # ax_1.plot(voltage_traces[channel_ids[34]][:1000], alpha=0.25)
-    # ax_1.spines['right'].set_visible(False)
-    # ax_1.spines['top'].set_visible(False)
-    # ax_1.spines['left'].set_visible(False)
-    # ax_1.spines['bottom'].set_visible(False)
-    # ax_1.axes.get_xaxis().set_visible(False)
-    # ax_1.axes.get_yaxis().set_visible(False)
-    # ax_1.axes.get_xaxis().set_ticks([])
-    # ax_1.axes.get_yaxis().set_ticks([])
-    # ax_1.patch.set_alpha(0.0)
+    plt.axis('off')
     if i > 0:
-        print(timepoints[i-1], timepoints[i])
-    for label in plot_positions.keys():
+        print(timepoints[i - 1], timepoints[i])
+    for j, label in enumerate(plot_positions.keys()):
         position = plot_positions[label]
         id = get_channel_id(label)
         ax_x = fig.add_axes([position[0], position[1], 0.05, 0.05])
         if i == 0:
             ax_x.plot(voltage_traces[id][:int(timepoints[i])], alpha=0.25, color='white')
             ax_x.text(position[0], position[1], label, color='white', fontsize=8)
-        ax_x.plot(voltage_traces[id][int(timepoints[i-1]):int(timepoints[i])], alpha=0.25, color='white')
+        if j == 6:
+            ax_x_props = ax_x.axis()
+            ax_x_xmin = ax_x_props[0]
+            ax_x_y_min = ax_x_props[2]
+            ax_x_x_scalebar = AnchoredSizeBar(ax_x.transData, 1, '0.1 s', loc='lower left', color='white', sep=2.5)
+            ax_x_y_scalebar = AnchoredSizeBar(ax_x.transData, 1, '', loc='lower left', color='white', sep=2.5)
+            ax_x.add_artist(ax_x_x_scalebar)
+            ax_x.add_artist(ax_x_y_scalebar)
+            ax_x.text(ax_x_xmin - 0.1, ax_x_y_min - 0.25, '1 uV', size=10, color='white', rotation='vertical')
+        ax_x.plot(voltage_traces[id][int(timepoints[i - 1]):int(timepoints[i])], alpha=0.25, color='white')
         ax_x.spines['right'].set_visible(False)
         ax_x.spines['top'].set_visible(False)
         ax_x.spines['left'].set_visible(False)
@@ -128,4 +115,5 @@ for i, mat in enumerate(mats):
         ax_x.patch.set_alpha(0.0)
         # ani = animation.ArtistAnimation(fig, img, interval=100, blit=True,
         # repeat_delay=1000)
-        plt.savefig('movie_' + str(i) + '.png')
+    plt.savefig('movie_' + str(i) + '.png')
+    print('figure', i, 'saved')
