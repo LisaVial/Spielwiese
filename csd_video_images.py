1ea7a67eda60f2e85761b8f276628a464e7157d0
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2021-01-11 10:12:21 +0100

    csd video scripts and script to make spectograms of channels used for the first csd video.

--- csd_video_images.py
+++ csd_video_images.py
@@ -6,6 +6,7 @@ import numpy as np
 import time
 # use agg backend due to performance issues
 matplotlib.use("agg")
+from IPython import embed
 
 
 def get_distance(label_a, label_b):
@@ -65,24 +66,7 @@ video_path = "/home/lisa_ruth/mea_recordings/CSD_data/201013_microscope" \
              "/FHM3_GS967_BL6_P15_male_exp100ms_dur100sec_400ms_7psi_Slice2_Test3_4movie.tif"
 
 mea_file = h5py.File(mea_recording_path, 'r')
-t0 = time.time()
-voltage_traces = mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData'][:]
-t1 = time.time() - t0
-print('time to load traces:', t1)
-# ids = [ch[1] for ch in mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']]
-# labels = [ch[4].decode('utf8') for ch in mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']]
-# same_len_labels = [str(label[0]) + '0' + str(label[1]) if len(label) < 3 else label for label in labels]
-# ordered_indices = list(np.argsort(same_len_labels))
-
-# channel_ids = list(np.asarray(ids)[ordered_indices])
-# print(channel_ids[49], labels[channel_ids[49]])
-
-t2 = time.time()
-# retvals, mats = cv2.imreadmulti(video_path, flags=(cv2.IMREAD_GRAYSCALE | cv2.IMREAD_ANYDEPTH))
 mats = tiff.imread(video_path)
-t3 = time.time() - t2
-print('time to open tiff stack:', t3)
-print(mats.shape)
 
 relevant_labels = ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'E3', 'E4',
                    'E5', 'E6', 'E7', 'E8', 'E9', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'G3', 'G4', 'G5', 'G6',
@@ -90,14 +74,23 @@ relevant_labels = ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D3', 'D4', 'D5', '
                    'K3', 'K4', 'K5', 'K6', 'K7', 'K8', 'K9', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'M3', 'M4', 'M5',
                    'M6', 'M7', 'M8', 'M9', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9']
 label = 'C3'
-x0, y0 = (0.175, 0.75)
+x0, y0 = (0.185, 0.7125)
 x_offset, y_offset = (0.06, -0.09)
 plot_positions = calculate_plot_positions(label, x0, y0, x_offset, y_offset, relevant_labels)
 
 stepsize = 1000
 num_frames = len(mats)
+print(stepsize*num_frames)
+voltage_traces = mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData'][:]
+# [:int(stepsize*num_frames)]
 timepoints = np.arange(stepsize, (num_frames * stepsize) + 1, stepsize)
-
+channels_voltage_traces = []
+for i, label in enumerate(relevant_labels):
+    id = get_channel_id(label)
+    voltage_trace_snippet = voltage_traces[id][:int(stepsize*num_frames)]
+    channels_voltage_traces.append(voltage_trace_snippet)
+ylim_0 = np.percentile(channels_voltage_traces, 2.5) - 1526.5
+ylim_1 = np.percentile(channels_voltage_traces, 97.5) + 1526.5
 for i, mat in enumerate(mats):
     fig = plt.figure(figsize=(12, 8))
     plt.imshow(mat, cmap='gray', interpolation='none')
@@ -106,21 +99,26 @@ for i, mat in enumerate(mats):
         print(timepoints[i - 1], timepoints[i])
     for j, label in enumerate(plot_positions.keys()):
         position = plot_positions[label]
-        id = get_channel_id(label)
         ax_x = fig.add_axes([position[0], position[1], 0.05, 0.05])
         if i == 0:
-            ax_x.plot(voltage_traces[id][:int(timepoints[i])], alpha=0.25, color='white')
+            ax_x.plot(channels_voltage_traces[j][:int(timepoints[i])], alpha=0.25, color='white')
             ax_x.text(position[0], position[1], label, color='white', fontsize=8)
         if j == 6:
-            ax_x_props = ax_x.axis()
-            ax_x_xmin = ax_x_props[0]
-            ax_x_y_min = ax_x_props[2]
-            ax_x_x_scalebar = AnchoredSizeBar(ax_x.transData, 1, '0.1 s', loc='lower left', color='white', sep=2.5)
-            ax_x_y_scalebar = AnchoredSizeBar(ax_x.transData, 1, '', loc='lower left', color='white', sep=2.5)
-            ax_x.add_artist(ax_x_x_scalebar)
-            ax_x.add_artist(ax_x_y_scalebar)
-            ax_x.text(ax_x_xmin - 0.1, ax_x_y_min - 0.25, '1 uV', size=10, color='white', rotation='vertical')
-        ax_x.plot(voltage_traces[id][int(timepoints[i - 1]):int(timepoints[i])], alpha=0.25, color='white')
+            matplotlib.rc('axes', edgecolor='white')
+            ax_scale = fig.add_axes([position[0]+0.00825, position[1]+(y_offset+0.055), 0.05, 0.05])
+            ax_scale.spines['right'].set_visible(False)
+            ax_scale.spines['top'].set_visible(False)
+            ax_scale.set_ylim(ylim_0, ylim_1)
+            ax_scale.set_xlim(0, 1000)
+            ax_scale.axes.get_xaxis().set_ticks([])
+            ax_scale.axes.get_yaxis().set_ticks([])
+            ax_scale.set_ylabel(r'%i $\mu$V' % (ylim_1-ylim_0))
+            ax_scale.set_xlabel('100 msecs')
+            ax_scale.yaxis.label.set_color('white')
+            ax_scale.xaxis.label.set_color('white')
+            ax_scale.patch.set_alpha(0.0)
+        ax_x.plot(channels_voltage_traces[j][int(timepoints[i - 1]):int(timepoints[i])], alpha=0.25, color='white')
+        ax_x.set_ylim(ylim_0, ylim_1)
         ax_x.spines['right'].set_visible(False)
         ax_x.spines['top'].set_visible(False)
         ax_x.spines['left'].set_visible(False)
@@ -134,3 +132,4 @@ for i, mat in enumerate(mats):
         # repeat_delay=1000)
     plt.savefig('movie_' + str(i) + '.png')
     print('figure', i, 'saved')
+
---
+++ spectogram.py
@@ -0,0 +1,35 @@
+import scipy.signal
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+
+
+def get_channel_id(current_label):
+    for ch in mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['InfoChannel']:
+        if current_label == ch[4].decode('utf8'):
+            return ch[1]
+
+
+mea_recording_path = "/home/lisa_ruth/mea_recordings/CSD_data/h5_data" \
+                     "/2020-10-13T14-27-38FHM3_GS967_BL6_P15_male_400ms_7psi_Slice3_Test1_WITH.h5"
+
+relevant_labels = ['C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'E3', 'E4',
+                   'E5', 'E6', 'E7', 'E8', 'E9', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'G3', 'G4', 'G5', 'G6',
+                   'G7', 'G8', 'G9', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9',
+                   'K3', 'K4', 'K5', 'K6', 'K7', 'K8', 'K9', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'M3', 'M4', 'M5',
+                   'M6', 'M7', 'M8', 'M9', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9']
+
+mea_file = h5py.File(mea_recording_path, 'r')
+voltage_traces = mea_file['Data']['Recording_0']['AnalogStream']['Stream_0']['ChannelData'][:]
+sampling_rate = 10000
+for idx, label in enumerate(relevant_labels):
+    id = get_channel_id(label)
+    signal = voltage_trace_snippet = voltage_traces[id][:10000] # only of the first second
+    f, t, Sxx = scipy.signal.spectrogram(signal, sampling_rate)
+    plt.pcolormesh(t, f, Sxx, shading='gouraud')
+    plt.ylim(0, 100)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.savefig('spectogram_' + str(idx) + '.png')
+    print('figure', idx, 'saved')
+
